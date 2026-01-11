0339da4699be32962f2cfa910e7f4fbcaa1b15b8
aarav505 <aaravanand2008@yahoo.com>
2026-01-11 13:21:12 +0530

    changed theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "../components/theme-provider";
+
 import { motion } from "framer-motion";
 import { ModeToggle } from "@/components/ModeToggle";
 
